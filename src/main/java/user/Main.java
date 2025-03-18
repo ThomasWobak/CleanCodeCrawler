e5ffc431a091fc7f124764ea8c192530b632d803
ThomasWobak <t1wobak@edu.aau.at>
2025-03-18 15:54:34 +0100

    Improved main Method

--- src/main/java/user/Main.java
+++ src/main/java/user/Main.java
@@ -2,9 +2,11 @@ package user;
 
 import crawler.Crawler;
 
+import java.util.Collections;
+
 public class Main {
     public static void main(String[] args) {
-        if(args.length!=2){
+        if(args.length!=3){
             System.out.println("Incorrect arguments");
             return;
         }
@@ -12,7 +14,7 @@ public class Main {
             String startUrl=args[0];
             int depth= Integer.parseInt(args[1]);
             String domain=args[2];
-            Crawler crawler=new Crawler(startUrl,depth,domain);
+            Crawler crawler=new Crawler(depth, Collections.singleton(domain), startUrl);
             crawler.crawl();
         }catch (Exception e) {
             System.out.println(e.getMessage());
